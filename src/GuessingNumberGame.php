f8bbf7c08c16586cae7b9048c6f52ab74eeca07d
JesusValera <jesus1902@outlook.com>
2022-10-03 18:07:08 +0200

    assert_player_initial_attempt_is_lower

--- src/GuessingNumberGame.php
+++ src/GuessingNumberGame.php
@@ -6,20 +6,24 @@ namespace GuessRandomNumber;
 
 final class GuessingNumberGame
 {
+    private const GUESSING_NUMBER = 5;
+
     public const STATUS_PLAYER_WIN = 'You win';
 
-    public const STATUS_LOWER = 'The number is lower';
+    public const STATUS_NUMBER_IS_LOWER = 'The number is lower';
 
-    public const STATUS_HIGHER = 'The number is higher';
+    public const STATUS_NUMBER_IS_HIGHER = 'The number is higher';
 
     public const STATUS_PLAYER_LOSES = 'You lost';
 
     public function guessNumber(int $number): string
     {
-        if ($number === 5) {
+        if ($number === self::GUESSING_NUMBER) {
             return self::STATUS_PLAYER_WIN;
         }
 
-        return self::STATUS_LOWER;
+        return self::GUESSING_NUMBER > $number
+            ? self::STATUS_NUMBER_IS_HIGHER
+            : self::STATUS_NUMBER_IS_LOWER;
     }
 }
--- tests/GuessingNumberGameTest.php
+++ tests/GuessingNumberGameTest.php
@@ -38,9 +38,26 @@ final class GuessingNumberGameTest extends TestCase
         $game = new GuessingNumberGame();
 
         $firstAttempt = $game->guessNumber(10);
-        self::assertEquals(GuessingNumberGame::STATUS_LOWER, $firstAttempt);
+        self::assertEquals(GuessingNumberGame::STATUS_NUMBER_IS_LOWER, $firstAttempt);
         $secondAttempt = $game->guessNumber(5);
         self::assertEquals(GuessingNumberGame::STATUS_PLAYER_WIN, $secondAttempt);
     }
 
+    /*
+     * As player
+     * I play a 3
+     * I play a 5
+     * the player wins
+     */
+
+    /** @test */
+    public function assert_player_initial_attempt_is_lower(): void
+    {
+        $game = new GuessingNumberGame();
+
+        $firstAttempt = $game->guessNumber(3);
+        self::assertEquals(GuessingNumberGame::STATUS_NUMBER_IS_HIGHER, $firstAttempt);
+        $secondAttempt = $game->guessNumber(5);
+        self::assertEquals(GuessingNumberGame::STATUS_PLAYER_WIN, $secondAttempt);
+    }
 }
