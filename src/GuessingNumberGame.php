473d6f8fbdacb1678fff73c1193bafac47e9a8fe
JesusValera <jesus1902@outlook.com>
2022-10-03 18:03:44 +0200

    assert_player_initial_attempt_is_higher

--- src/GuessingNumberGame.php
+++ src/GuessingNumberGame.php
@@ -6,8 +6,20 @@ namespace GuessRandomNumber;
 
 final class GuessingNumberGame
 {
-    public function guessNumber(int $number): bool
+    public const STATUS_PLAYER_WIN = 'You win';
+
+    public const STATUS_LOWER = 'The number is lower';
+
+    public const STATUS_HIGHER = 'The number is higher';
+
+    public const STATUS_PLAYER_LOSES = 'You lost';
+
+    public function guessNumber(int $number): string
     {
-        return true;
+        if ($number === 5) {
+            return self::STATUS_PLAYER_WIN;
+        }
+
+        return self::STATUS_LOWER;
     }
 }
--- tests/GuessingNumberGameTest.php
+++ tests/GuessingNumberGameTest.php
@@ -22,6 +22,25 @@ final class GuessingNumberGameTest extends TestCase
 
         $result = $game->guessNumber(5);
 
-        self::assertEquals(true, $result);
+        self::assertEquals(GuessingNumberGame::STATUS_PLAYER_WIN, $result);
     }
+
+    /*
+     * As player
+     * I play a 10
+     * I play a 5
+     * the player wins
+     */
+
+    /** @test */
+    public function assert_player_initial_attempt_is_higher(): void
+    {
+        $game = new GuessingNumberGame();
+
+        $firstAttempt = $game->guessNumber(10);
+        self::assertEquals(GuessingNumberGame::STATUS_LOWER, $firstAttempt);
+        $secondAttempt = $game->guessNumber(5);
+        self::assertEquals(GuessingNumberGame::STATUS_PLAYER_WIN, $secondAttempt);
+    }
+
 }
